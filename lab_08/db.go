0d72f28b2ed81a682be62dc7b52cb78813f13a11
Maksim Yatsenyuk <yaesenykmax356@gmail.com>
2026-05-09 16:37:31 +0300

    Update lab8; Added Docker

--- lab_08/db.go
+++ lab_08/db.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"log"
+	"os"
+	"time"
 
 	_ "github.com/lib/pq"
 )
@@ -10,15 +13,31 @@ import (
 var DB *sql.DB
 
 func connectDB() {
-	connStr := "postgres://localhost:5432/contacts_db?sslmode=disable"
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	dbname := os.Getenv("DB_NAME")
+
+	connStr := fmt.Sprintf(
+		"postgres://%s:%s@%s:%s/%s?sslmode=disable",
+		user, password, host, port, dbname,
+	)
 
 	var err error
-	DB, err = sql.Open("postgres", connStr)
-	if err != nil {
-		log.Fatal(err)
+
+	for i := 0; i < 10; i++ {
+		DB, err = sql.Open("postgres", connStr)
+		if err == nil {
+			err = DB.Ping()
+			if err == nil {
+				return
+			}
+		}
+
+		log.Println("DB not ready, retrying...")
+		time.Sleep(2 * time.Second)
 	}
 
-	if err = DB.Ping(); err != nil {
-		log.Fatal(err)
-	}
+	log.Fatal("Cannot connect to DB:", err)
 }
